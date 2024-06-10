b437a3834a0b0025ab74b7fa56a6ca57fcdb8c4f
Daniel Owen <unlimiteddemi@gmail.com>
2024-06-10 19:09:55 +0100

    new

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ from logger import logging
 def error_message_detail(error ,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()  
     file_name  = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in Python Script name [{0}] line number [{1}] error_message[{2}]".format(file_name , exc_tb.tb_lineno,str(error)
+    error_message = "Error occurred in Python Script name [{0}] line number [{1}] error_message[{2}]".format(file_name , exc_tb.tb_lineno,str(error)
     )
     
 
