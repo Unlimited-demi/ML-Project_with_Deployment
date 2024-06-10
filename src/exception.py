4e11455fe38b05536c74f53250df98412fe22ccd
Daniel Owen <unlimiteddemi@gmail.com>
2024-06-10 18:41:34 +0100

    logging in exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from logger import logging
+
 
 def error_message_detail(error ,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()  
@@ -17,7 +18,6 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-
 
 
 
