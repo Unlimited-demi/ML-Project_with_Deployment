1e2c03c133371460be3d2d83f2c49bad4a9fd078
Daniel Owen <unlimiteddemi@gmail.com>
2024-06-16 00:35:23 +0100

    Beanstalk Deployment

--- application.py
+++ application.py
@@ -41,5 +41,5 @@ def predict_datapoint():
         result  = predict_pipeline.predict(pred_df)
         return render_template('home.html'  , results = result[0])
 if __name__ == "__main__":
-        app.run(host='0.0.0.0' , debug= True)
+        app.run(host='0.0.0.0')
 
